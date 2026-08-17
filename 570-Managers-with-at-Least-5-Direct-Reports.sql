00d25f42474d454b8230143864f25946e17c492c
shivam-jund <shivamjund@gmail.com>
2026-08-17 16:42:10 +0530

    [570] [Time Beats: 69.69%] - LeetPush

---
+++ 570-Managers-with-at-Least-5-Direct-Reports.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+
+select name 
+from employee
+where id in 
+(
+    select managerId
+    from employee
+    group by managerId 
+    having count(*) >= 5
+);
