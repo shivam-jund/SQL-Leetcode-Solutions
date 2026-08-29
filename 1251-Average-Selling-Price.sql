b6f261893a35d3c773639bcdf1ec057468094939
shivam-jund <shivamjund@gmail.com>
2026-08-29 12:13:21 +0530

    [1251] [Time Beats: 66.83%] - LeetPush

---
+++ 1251-Average-Selling-Price.sql
@@ -0,0 +1,4 @@
+select p.product_id, coalesce(round(sum(p.price*u.units)::numeric/sum(units),2),0) as average_price
+from Prices p
+left join UnitsSold u on p.product_id = u.product_id and (u.purchase_date between p.start_date and p.end_date)
+group by p.product_id;
